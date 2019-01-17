93d4ed59fe8d43c21745cf0f160778a5b19520ef
Jose Ángel Gumiel <jagumiel001@gmail.com>
2019-01-17 22:15:03 +0100

    Simplified
    Unnecesary libraries where removed.
    Code is now more clean and efficient.

--- 10-counter/counter.vhd
+++ 10-counter/counter.vhd
@@ -1,8 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
-use IEEE.numeric_std.all;
-
+USE IEEE.STD_LOGIC_ARITH.ALL;
 
 entity counter is
     Generic(
@@ -15,26 +13,23 @@ entity counter is
 end counter;
 
 architecture Behavioral of counter is
-
-    signal data : std_logic_vector(nb downto 0):=((others=>'0'));
-    signal limite : std_logic_vector(nb downto 0);
+    
+	signal data : UNSIGNED(nb downto 0):=((others=>'0'));
 
 begin
-    limite <= std_logic_vector(to_unsigned(nc, nb+1));
 
     process(clk)
     begin
         if(rising_edge(clk))then
-            if(data<limite)then
+			if (rst='1')then
+				data<=((others=>'0'));
+            elsif(data<(UNSIGNED(CONV_STD_LOGIC_VECTOR(nc, nb+1))))then
                 data<=data+'1';
             else
                 data<=((others=>'0'));
             end if;
         end if;
-        if (rst='1')then
-            data<=((others=>'0'));
-        end if;
     end process;
-    dat<=data;
+    dat<=std_logic_vector(data);
 
 end Behavioral;
